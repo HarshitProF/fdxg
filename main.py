5dfd388fef30a6fbc3409463781edc859dd45332
HarshitProF <121898573+HarshitProF@users.noreply.github.com>
2023-01-08 21:15:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,16 @@ def send2():
         print(json_string)
         update=Update.de_json(json_string)
         jsot=json.loads(json_string)['message']['text'].split('\n')
-        print(jsot)
+        data={
+            "dat":jsot[0],
+            "pair":jost[2].split(":")[1],
+            "type":jost[3],
+            "leverage":jost[4].split(":")[1],
+            "entery":jost[5].split(":")[1],
+            "target":[jost[7].split(":")[1],jost[8].split(":")[1],jost[9].split(":")[1],jost[10].split(":")[1],jost[11].split(":")[1],jost[12].split(":")[1],jost[13].split(":")[1]]
+            "stoploss":jost[15].split(":")[1]
+        }
+        print(data)
         return ''
 if __name__=='__main__':
     app.run()
