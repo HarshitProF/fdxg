57d708f25590941076d25a6b312b7749038cd20c
HarshitProF <121898573+HarshitProF@users.noreply.github.com>
2023-01-08 21:56:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,16 +11,20 @@ def send2():
         print(json_string)
         update=Update.de_json(json_string)
         jsot=json.loads(json_string)['message']['text'].split('\n')
+        chat_id=json.loads(json_string)['message']['from']['id']
         data={
             'dat':jsot[0],
             'pair':jsot[2].split(":")[1],
             'type':jsot[3],
             'leverage':jsot[4].split(":")[1],
-            'entery':jsot[5].split(":")[1],
-            'target':[float(jsot[7].split(":")[1]),float(jsot[8].split(":")[1]),float(jsot[9].split(":")[1]),float(jsot[10].split(":")[1]),float(jsot[11].split(":")[1]),float(jsot[12].split(":")[1]),float(jsot[13].split(":")[1])],
+            'entry':jsot[5].split(":")[1],
+            'targets':[float(jsot[7].split(":")[1]),float(jsot[8].split(":")[1]),float(jsot[9].split(":")[1]),float(jsot[10].split(":")[1]),float(jsot[11].split(":")[1]),float(jsot[12].split(":")[1]),float(jsot[13].split(":")[1])],
             'stoploss':jsot[15].split(":")[1]
         }
-        print(data)
+        message1=f"✨{data['pair']}\n\n🎗 Trade Type={data['type']}\n\n💫 Leverage={data['leverage']}\n\n⚡️ Entry={data['entry']}\n\n❌ StopLoss={data['stoploss']}\n\n❎ Take profit={data['tagets']}"
+        url=f"https://api.telegram.org/bot{apikey}/sendMessage?chat_id={chat_id}&text={message1}"
+        result=requests.get(url)
+        print(result.text)
         return ''
 if __name__=='__main__':
     app.run()
